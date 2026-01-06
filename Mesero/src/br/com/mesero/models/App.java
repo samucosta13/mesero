e40afecf0931616cd89211983922ec1c43ea23ab
Samuel Felipe <costasamuelf7@gmail.com>
2026-01-06 00:47:07 -0300

    Comment

--- Mesero/src/br/com/mesero/models/App.java
+++ Mesero/src/br/com/mesero/models/App.java
@@ -1,5 +1,11 @@
 package br.com.mesero.models;
 
+/*
+Model → representa o domínio
+DAO → persiste objetos
+Service → aplica regras
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
